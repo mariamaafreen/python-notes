19bcce395cca004bae03431040bac0d5e924e753
Mariam <sanubia99@gmail.com>
2019-06-23 16:27:15 +0530

    list sort

---
+++ sort_list.py
@@ -0,0 +1,9 @@
+numbers = [1,9,3,100]
+
+numbers.sort(reverse = True)
+print("the numbers sorted in decending order are:")
+print(numbers)
+
+numbers.sort()
+print("the numbers sorted in ascending order are:")
+print(numbers)
